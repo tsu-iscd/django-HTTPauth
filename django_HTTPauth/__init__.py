819286c59c2fc741c52a98077b666a4467ca040e
afr1ka <afr1ka@afr1ka-url.(none)>
2014-05-19 22:27:14 +0700

    Django_HTTPauth application has been added.

---
+++ django_HTTPauth/__init__.py
@@ -0,0 +1,165 @@
+from django.shortcuts import render, render_to_response
+from django import forms
+from django.utils.crypto import get_random_string
+from django.conf import settings
+
+import hashlib
+import hmac
+import simplejson
+import re
+import base64
+
+from Crypto.Cipher import AES
+from Crypto import Random
+
+import time
+SCSRF_RAND_LENGTH = 16
+
+BLOCK_SIZE = 16
+
+def token_validate(request,token,dic_all):
+
+    if len(dic_all)==0:
+        return False
+
+    policy = {}
+    b_key = ''
+    sts=''
+
+    for polid in request.session['forms'].keys():
+        if re.sub(base64.b64decode(polid),'#',request.build_absolute_uri())=='#':
+            policy = request.session['forms'][polid]['policy']
+            b_key = request.session['forms'][polid]['auth_key']
+            sts = request.session['forms'][polid]['time']
+            objid = polid
+
+
+    if len(policy)==0 and len(b_key)==0:
+        for k in request.session['csrf_tokens']:
+            if k == token:
+                print 'Val - '+k
+                return True
+        return False
+
+    ncont=policy['name_protection']
+    rprot = policy['replay_protection']
+    res_str = ''
+
+    if rprot['enable']==True:
+        time_interval = rprot['interval']
+        tstamp = int(sts) 
+        if int(round(time.time()*1000)) - tstamp >= int(time_interval*1000):
+            return False
+    
+    fl_np = []
+    if ncont == True:
+        fl_np = [k for k in dic_all]
+        fl_np.sort()
+        res_str += '&'.join(fl_np)+';'
+
+    fl_val = []
+    for fl in policy['parameter_protection']:
+        if policy['parameter_protection'][fl]['action'] == 'validate':
+            try:
+                val_res = re.sub(policy['parameter_protection'][fl]['value'],'#',dic_all[fl])
+                fl_val.append(fl+'='+val_res)
+            except KeyError:
+                return False
+        elif policy['parameter_protection'][fl]['action']=='control':
+            try:
+                fl_val.append(fl+'='+dic_all[fl])
+            except KeyError:
+                return False
+ 
+    fl_val.sort()
+    res_str+='&'.join(fl_val)
+
+    try:
+        subid = ''
+        if policy.has_key('subject')==True:
+            subid = request.COOKIES[policy['subject']]
+        else:
+            subid = request.COOKIES[settings.SESSION_COOKIE_NAME]
+    except KeyError:
+        return False
+
+    res_token =res_str+';'+sts+';'+objid+';'+subid+';'+request.method
+    print 'Validation: '+res_token
+
+    sc_tok = sfunc_mess(base64.b64decode(b_key),res_token)
+    
+    if sc_tok == token:
+        return True
+
+    return False
+
+
+def sfunc_mess(key,message):
+    shash = hmac.new(key,msg=message,digestmod=hashlib.sha256).digest()
+    return base64.b64encode(shash).decode()
+
+
+def auth_render(request, *args, **kwargs):
+    request.session['forms']={}
+    request.session['csrf_tokens']=[]
+    for cont in args:
+        if type(cont) is dict:
+            for key in cont:
+                if issubclass(cont[key].__class__,forms.ModelForm)==True or issubclass(cont[key].__class__,forms.Form)==True:
+                    f = cont[key]
+
+                    try:
+
+                        tstamp=''
+                        if f.policy.get('replay_protection')['enable']==True:
+                            tstamp = ';'+str(int(round(time.time()*1000)))
+                        rndfile = Random.new()
+                        gen_key = rndfile.read(BLOCK_SIZE)
+                        b_key=base64.b64encode(gen_key)
+
+                        lf_all = []
+                        for x in f.fields:
+                            lf_all.append(x)
+                        lf_all.sort()
+                        
+                        res_str = ''
+                        if f.policy.get('name_protection') == True:
+                            res_str = '&'.join(lf_all)
+                            res_str+=';'
+
+                        fl_val = []
+                        for fl in f.policy['parameter_protection']:
+                            if f.policy['parameter_protection'][fl]['action'] == 'validate':
+                                fl_val.append(fl+'=#')
+                            elif f.policy['parameter_protection'][fl]['action']=='control':
+                                fl_val.append(fl+'='+f.policy['parameter_protection'][fl]['value'])
+
+                        fl_val.sort()
+
+                        request.session['forms'][base64.b64encode(f.policy['object'])]={'policy':f.policy,'auth_key':b_key,'time':tstamp[1:]}                        
+                        res_str+='&'.join(fl_val)
+
+                        try:
+                            subid = ''
+                            if f.policy.has_key('subject')==True:
+                                subid = request.COOKIES[f.policy['subject']]
+                            else:
+                                subid = request.COOKIES[settings.SESSION_COOKIE_NAME]
+                        except KeyError:
+                            subid = request.session.session_key
+
+                        res_token=res_str+tstamp+';'+base64.b64encode(f.policy['object'])+';'+subid+';POST'
+
+                        print 'Generation: '+res_token
+
+                        sc_tok = sfunc_mess(gen_key,res_token)
+                        f.fields["auth_token"]= forms.CharField(widget=forms.HiddenInput,max_length=len(sc_tok),initial=sc_tok)
+                    except AttributeError:
+                        res_token = get_random_string(SCSRF_RAND_LENGTH)
+                        print 'Gen - '+res_token
+                        f.fields["auth_token"]= forms.CharField(widget=forms.HiddenInput,max_length=len(res_token),initial=res_token)
+                        request.session['csrf_tokens'].append(res_token)
+                        	
+    return render(request, *args, **kwargs)
+
+
---
+++ django_HTTPauth/middleware.py
@@ -0,0 +1,96 @@
+"""
+Cross Site Request Forgery Middleware.
+
+This module provides a middleware that implements protection
+against request forgeries from other sites.
+"""
+from __future__ import unicode_literals
+
+import logging
+import itertools
+import re
+import django_HTTPauth
+import copy
+
+from django.conf import settings
+from django.core.urlresolvers import get_callable
+from django.utils.cache import patch_vary_headers
+from django.utils.encoding import force_text
+from django.utils.http import same_origin
+from django.utils.crypto import constant_time_compare, get_random_string
+from django.utils.safestring import mark_safe
+
+
+logger = logging.getLogger('django.request')
+
+REASON_NO_REFERER = "Referer checking failed - no Referer."
+REASON_BAD_REFERER = "Referer checking failed - %s does not match %s."
+REASON_NO_CSRF_COOKIE = "CSRF cookie not set."
+REASON_BAD_TOKEN = "CSRF token missing or incorrect."
+
+
+
+def _get_failure_view():
+    return get_callable(settings.CSRF_FAILURE_VIEW)
+
+
+def _get_new_csrf_key():
+    return get_random_string(CSRF_KEY_LENGTH)
+
+
+
+
+def validate_auth_token(request,auth_token):
+
+    dic_all = {} 
+    for x in request.POST:
+        if x!='auth_token' and x!='auth_policy':
+            dic_all[x]=request.POST[x]
+
+
+    if django_HTTPauth.token_validate(request,auth_token,dic_all) == False:
+        return False
+    return True
+ 
+def _sanitize_token(token):
+    token = re.sub('[^a-zA-Z0-9+/=]+', '', force_text(token))
+    if token == '':
+        return None
+    return token
+
+class HttpAuthMiddleware(object):
+    def _accept(self, request):
+        return None
+
+    def _reject(self, request, reason):
+        logger.warning('Forbidden (%s): %s',
+                       reason, request.path,
+            extra={
+                'status_code': 403,
+                'request': request,
+            }
+        )
+        return _get_failure_view()(request, reason=reason)
+
+    def process_view(self, request, callback, callback_args, callback_kwargs):
+
+        if getattr(request, 'auth_processing_done', False):
+            return None
+
+        if request.method == 'POST':
+            auth_token = _sanitize_token(request.POST.get('auth_token',''))
+            
+            if auth_token is None:
+                return self._reject(request, REASON_NO_CSRF_COOKIE)
+            
+            if auth_token == "":
+                return self._reject(request, REASON_BAD_TOKEN)
+
+            if not validate_auth_token(request,auth_token):
+                return self._reject(request, REASON_BAD_TOKEN)
+
+        return self._accept(request)
+
+
+
+
