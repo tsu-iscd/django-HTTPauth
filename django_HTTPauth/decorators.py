ada4a1dea454a886163d5ef25b977a0573a9aee9
afr1ka <n.o.tkachenko@gmail.com>
2014-06-25 00:18:27 +0700

    Policy_name_protection(boolean) decorator which adds name_protection parameter to policy of the form has been added.

---
+++ django_HTTPauth/decorators.py
@@ -0,0 +1,16 @@
+
+class policy_name_protection():
+    def __init__(self,name_protection):
+        self.np=name_protection
+        
+    def __call__(self,target): 
+        try:
+            if target.policy.has_key('name_protection')==True:
+                if target.policy.get('name_protection')==False and self.np==True:
+                    target.policy['name_protection']=self.np
+            else:
+                target.policy['name_protection']=self.np
+        except AttributeError:
+            target.policy={'name_protection',self.np}
+        return target
+    
