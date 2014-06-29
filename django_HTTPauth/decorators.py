d0aeb138a5ab3cc16fde67c8b8b129c639355d8c
afr1ka <n.o.tkachenko@gmail.com>
2014-06-30 00:44:34 +0700

    Added decorator policy_replay_protection.

--- django_HTTPauth/decorators.py
+++ django_HTTPauth/decorators.py
@@ -14,3 +14,28 @@ class policy_name_protection():
             target.policy={'name_protection':self.np}
         return target
     
+
+class policy_replay_protection():
+    def __init__(self,replay_time,replay_protection=True):
+        self.rp=replay_protection
+        if type(replay_time) is int:
+            if replay_time>0:
+                self.rt=replay_time
+            else:
+                self.rt=0
+        elif replay_time.isdigit() == True:
+            self.rt=int(replay_time)
+        else:
+            self.rt=0
+            self.rp=False
+        
+    def __call__(self,target): 
+        try:
+            if target.policy.has_key('replay_protection')==True:
+                    target.policy['replay_protection']['enable']=self.rp
+                    target.policy['replay_protection']['interval']=self.rt
+            else:
+                target.policy['name_protection']={'enable':self.rp,'interval':self.rt}
+        except AttributeError:
+            target.policy={'name_protection':{'enable':self.rp,'interval':self.rt}}
+        return target
